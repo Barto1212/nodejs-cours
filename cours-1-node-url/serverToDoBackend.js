7f1c0b4b3c1ed9bdc1c96f93ebac0a1313cbf4a9
Barto1212 <loic_barthomeuf@hotmail.fr>
2022-11-02 14:03:51 +0100

    erreurs cors ?

--- cours-1-node-url/serverToDoBackend.js
+++ cours-1-node-url/serverToDoBackend.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const url = require('url')
 
 const textContent = {'Content-Type': 'text/plain; charset=UTF-8', "Access-Control-Allow-Origin": "*" }
 const jsonContent = { 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*" }
@@ -8,23 +9,43 @@ const toDoListBDD = [
   'Regarder une série'
 ]
 
-const serverToDoBackend = http.createServer((req, res) => {
-  const  { url, method } = req
-  if (method === 'GET') {
-    if (url === '/todos') {
-      res.writeHead(200,  jsonContent)
-      res.end(JSON.stringify(toDoListBDD))
-    } else {
-      res.writeHead(404, textContent)
-      res.end('non trouvé')
-    }
-    return
+const getRequest = (req, res) => {
+  if (req.url === '/todos') {
+    res.writeHead(200,  jsonContent)
+    res.end(JSON.stringify(toDoListBDD))
+  } else {
+    res.writeHead(404, textContent)
+    res.end('non trouvé')
   }
-  if (method === 'PUT') {
-    res.writeHead(201).end('ok')
-  }
+}
+
+const handle404 = (req, res) => {
   res.writeHead(404, textContent)
   res.end('Uniquement des requètes get')
+}
+
+const putRequest = (req, res) => {
+  const clientUrl = url(req)
+  console.log(clientUrl);
+  res.writeHead(201).end('ok')
+}
+
+const serverToDoBackend = http.createServer((req, res) => {
+  switch (req.method) {
+    case 'GET':
+      getRequest(req, res)
+      break
+  
+    case 'PUT':
+      putRequest(req, res)
+      break
+    case 'OPTION':
+      console.log('option');
+
+    default:
+      handle404(req, res)
+      break;
+  }
 })
 
 module.exports = serverToDoBackend
--- cours-1-node-url/staticServer.js
+++ cours-1-node-url/staticServer.js
@@ -1,14 +1,22 @@
 const fs = require('fs/promises')
 const http = require('http')
 
-const contenuTexte = {'Content-Type': 'text/plain; charset=UTF-8' }
+const cors = { "Access-Control-Allow-Origin": "*" }
+const contenuTexte = { 'Content-Type': 'text/plain; charset=UTF-8', ...cors }
 
 staticServer = http.createServer((req, res) => {
   const  { url, method } = req
   if (method === 'GET') {
+    if (url === '/') { 
+      const redirectURL = new URL("http://localhost")
+      redirectURL.pathname = "/index"
+      res.writeHead(301, { "Location": redirectURL, ...cors })
+      res.end()
+      return
+     }
     fs.readFile(`./public${url}.html`)
       .then(data => {
-        res.writeHead(200, {'Content-Type': 'text/html' })
+        res.writeHead(200, {'Content-Type': 'text/html', ...cors })
         res.end(data)
       })
       .catch(err =>{
