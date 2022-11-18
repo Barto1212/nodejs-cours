492807d1e52dbc7366c3d59f867b42a59ba6b418
Barto1212 <loic_barthomeuf@hotmail.fr>
2022-11-18 15:46:54 +0100

    add favicon & css

--- 1-8_p80/static.js
+++ 1-8_p80/static.js
@@ -1,38 +1,42 @@
-const fs = require('fs/promises')
-const http = require('http')
+const fs = require("fs/promises");
+const http = require("http");
 
-const cors = { "Access-Control-Allow-Origin": "*" }
-const contenuTexte = { 'Content-Type': 'text/plain; charset=UTF-8', ...cors }
+const cors = { "Access-Control-Allow-Origin": "*" };
+const contenuTexte = { "Content-Type": "text/plain; charset=UTF-8", ...cors };
 
 staticServer = http.createServer((req, res) => {
-  const  { url, method } = req
-  if (method === 'GET') {
-    if (url === '/') { 
-      const redirectURL = new URL("http://localhost")
-      redirectURL.pathname = "/index.html"
-      redirectURL.port = 3000
-      res.writeHead(301, { "Location": redirectURL, ...cors })
-      res.end()
-      return
-     }
-    fs.readFile(`./public${url}`)
-      .then(data => {
-        res.writeHead(200, {'Content-Type': 'text/html', ...cors })
-        res.end(data)
-      })
-      .catch(err =>{ 
-        if (err.errno === -2) {
-          res.writeHead(404, contenuTexte)
-          res.end('Page non trouvée')
-        } else {
-          res.writeHead(500, contenuTexte)
-          res.end('Erreur serveur')
-        }
-      })
-      return
+  const { url, method } = req;
+  if (method === "GET") {
+    if (url === "/") {
+      const redirectURL = new URL("http://localhost");
+      redirectURL.pathname = "/index.html";
+      redirectURL.port = 3000;
+      res.writeHead(301, { Location: redirectURL, ...cors });
+      res.end();
+      return;
     }
-  res.writeHead(404, contenuTexte)
-  res.end('Uniquement des requètes get')
-})
+    fs.readFile(`./public${url}`)
+      .then((data) => {
+        if (url.includes("css")) {
+          res.writeHead(200, { "Content-Type": "text/css", ...cors });
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html", ...cors });
+        }
+        res.end(data);
+      })
+      .catch((err) => {
+        if (err.errno === -2) {
+          res.writeHead(404, contenuTexte);
+          res.end("Page non trouvée");
+        } else {
+          res.writeHead(500, contenuTexte);
+          res.end("Erreur serveur");
+        }
+      });
+    return;
+  }
+  res.writeHead(404, contenuTexte);
+  res.end("Uniquement des requètes get");
+});
 
-module.exports = staticServer
+module.exports = staticServer;
