d89d1a59347e463d2c0506c05a65d788d9132bde
Barto1212 <loic_barthomeuf@hotmail.fr>
2022-12-05 10:31:47 +0100

    post ok p39

--- 3-4_39/routes/allRoutes.js
+++ 3-4_39/routes/allRoutes.js
@@ -24,20 +24,69 @@ const BDD = {
   ],
   cart: [],
 };
+// ----------------------------------      GET /articles/all     ----------------------------------
 router.get("/articles/all", (req, res) => {
   res.status(200).json(BDD.articles);
 });
+// ----------------------------------      GET /cart/all     ----------------------------------
 router.get("/cart/all", (req, res) => {
   res.status(200).json(BDD.cart);
 });
-router.get("/articles/:id", (req, res) => {
-  const { id } = req.params; // req.params = {id: dezf, name: fr}
-  const article = BDD.articles.find((element) => element.id == id);
+// ----------------------------------      GET /articles/{:articlesId}      ----------------------------------
+router.get("/articles/:articleId", (req, res) => {
+  const { articleId } = req.params; // req.params = {id: dezf, name: fr}
+  const article = BDD.articles.find((element) => element.id == articleId);
   if (article === undefined) {
     res.status(404).send("objet non trouvé");
     return;
   }
   res.status(200).send(article);
+});
+// ----------------------------------      DELETE /articles/{:articlesId}      ----------------------------------
+
+router.delete("/articles/:articleId", (req, res) => {
+  const { articleId } = req.params;
+  const indexToDelete = BDD.articles.findIndex(
+    (element) => element.id == articleId
+  );
+  if (indexToDelete === -1) {
+    res.status(404).send("objet non trouvé");
+    return;
+  }
+  const deletedArticle = BDD.articles[indexToDelete];
+  BDD.articles.splice(indexToDelete, 1);
+  res.status(200).send(deletedArticle);
+});
+
+// ----------------------------------      POST /articles      ----------------------------------
+router.post("/articles", (req, res) => {
+  const newArticle = req.body;
+  // Tests sur le format :
+  const keys = Object.keys(newArticle);
+  if (
+    keys.includes("id") &&
+    keys.includes("name") &&
+    keys.includes("description") &&
+    keys.includes("price")
+  ) {
+    if (typeof newArticle.id !== "number") {
+      res.status(405).send("Type incorrect");
+    }
+    if (typeof newArticle.name !== "string") {
+      res.status(405).send("Type incorrect");
+    }
+    if (typeof newArticle.description !== "string") {
+      res.status(405).send("Type incorrect");
+    }
+    if (typeof newArticle.price !== "number") {
+      res.status(405).send("Type incorrect");
+    }
+    // Cas sans erreurs
+    BDD.articles.push(newArticle);
+    res.sendStatus(201);
+  } else {
+    res.status(405).send("Champ manquant");
+  }
 });
 
 export default router;
